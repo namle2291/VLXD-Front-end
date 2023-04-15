d932f841ef541248ea90cc4b9a3bf6cb1f51adc3
lnam6507 <lnam6507@gmail.com>
2023-04-15 22:29:04 +0700

    update

--- index.js
+++ index.js
@@ -30,6 +30,22 @@ $(document).ready(function () {
                 }
             }
         ],
-        appendDots: $('.slick-slider-dots'),
     });
 });
+
+$(document).ready(function () {
+    $('.post_list').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('#post_prev_btn'),
+        nextArrow: $('#post_next_btn'),
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+});
+
+$('.btn_go_to_top').on('click',()=>{
+    $(window).scrollTop(0);
+})
