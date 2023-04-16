33cbcb67a87a931b9109ca7912a066051262d44d
lnam6507 <lnam6507@gmail.com>
2023-04-16 11:38:12 +0700

    responsive

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
 $(document).ready(function () {
-    $('.product_slider').slick({
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        prevArrow: "<button class='btn btn-sm'><i class='fas fa-angle-left'></i></button>",
-        nextArrow: "<button class='btn btn-sm'><i class='fas fa-angle-right'></i></button>",
-        adaptiveHeight: true,
-        responsive: [{
-                breakpoint: 1240,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ],
-    });
-});
-
-$(document).ready(function () {
     $('.post_list').slick({
         infinite: true,
         slidesToShow: 1,
@@ -46,6 +11,6 @@ $(document).ready(function () {
     });
 });
 
-$('.btn_go_to_top').on('click',()=>{
+$('.btn_go_to_top').on('click', () => {
     $(window).scrollTop(0);
 })
